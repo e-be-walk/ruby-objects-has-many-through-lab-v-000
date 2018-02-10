35c31c1019b8664821936dab9ab98cb055cb4b1e
Erin Walker <erinb0317@hotmail.com>
2018-02-10 13:17:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,13 @@
 class Artist
+  attr_accessor :name 
 
   def initialize(name)
     @name = name
     @songs = []
-  end 
+  end
 
   def add_song(song)
-    @songs << song 
+    @songs << song
     song.artist = self
   end
 
